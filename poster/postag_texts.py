397ca9724b9e9f5a01349d6de4dfc1dc9561071e
Rebecca Bilbro <rebecca.bilbro@bytecubed.com>
2017-05-12 12:20:48 -0400

    adding texts for postag visualization

---
+++ poster/postag_texts.py
@@ -0,0 +1,38 @@
+nursery_rhyme = '''Baa, baa, black sheep,
+                   Have you any wool?
+                   Yes, sir, yes, sir,
+                   Three bags full;
+                   One for the master,
+                   And one for the dame,
+                   And one for the little boy
+                   Who lives down the lane.'''
+
+algebra       = '''Algebra (from Arabic "al-jabr" meaning
+                   "reunion of broken parts") is one of the
+                   broad parts of mathematics, together with
+                   number theory, geometry and analysis. In
+                   its most general form, algebra is the study
+                   of mathematical symbols and the rules for
+                   manipulating these symbols; it is a unifying
+                   thread of almost all of mathematics.'''
+
+french_silk   = '''In a small saucepan, combine sugar
+                   and eggs until well blended. Cook
+                   over low heat, stirring constantly,
+                   until mixture reaches 160° and coats
+                   the back of a metal spoon. Remove
+                   from the heat. Stir in chocolate and
+                   vanilla until smooth. Cool to
+                   lukewarm (90°), stirring occasionally.
+                   In a small bowl, cream butter until
+                   light and fluffy. Add cooled
+                   chocolate mixture; beat on high speed
+                   for 5 minutes or until light and
+                   fluffy. In another large bowl, beat
+                   cream until it begins to thicken. Add
+                   confectioners' sugar; beat until stiff
+                   peaks form. Fold into chocolate
+                   mixture. Pour into crust. Chill for at
+                   least 6 hours before serving. Garnish
+                   with whipped cream and chocolate curls
+                   if desired.'''
